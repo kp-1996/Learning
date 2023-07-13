b80af8048abe250b6caee6d1b370633ffae1fd23
Tulasi Prasad <tulasiprasad@Tulasis-MacBook-Air.local>
2023-07-13 16:51:48 +0530

    jenkins script changes

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,9 @@
 pipeline {
-    agent { docker { image 'maven:3.9.3-eclipse-temurin-11' } }
     stages {
         stage('build') {
             steps {
                 sh 'mvn --version'
+		sh 'man clean build'
             }
         }
     }
